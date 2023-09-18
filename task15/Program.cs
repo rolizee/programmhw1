e5a09820819304df5ea6bc9f9888f7bc5cf12942
rolizee <134880152+rolizee@users.noreply.github.com>
2023-09-18 18:47:24 +0300

    new edit

---
+++ task15/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("This is not a weekday");
+    return false;
+}
+int weekDay = Prompt("Enter a weekday >");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Finally it's weekend");
+    }
+    else
+    {
+        Console.WriteLine("You'll have to work");
+    }
+}
