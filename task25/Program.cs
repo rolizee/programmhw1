aa2de7c277a7215a98a45b10a8016154e51b9b97
rolizee <134880152+rolizee@users.noreply.github.com>
2023-09-20 14:56:07 +0300

    Решение задачи 25

---
+++ task25/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message) ; //Выводим приглашение ко вводу
+    string readInput = System.Console.ReadLine() ; //Вводим значение
+    int result = int.Parse(readInput); //Приводим к числу
+    return result; //Возвращаем результат
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показательне должен быть меньше нуля");
+        return false;
+    }
+}
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
