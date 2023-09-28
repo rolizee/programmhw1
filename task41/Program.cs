0875d46167d5117fc22487073a116213db44496d
rolizee <134880152+rolizee@users.noreply.github.com>
2023-09-28 16:56:46 +0300

    Task 41 done

---
+++ task41/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+//Считать число с консоли
+int Prompt(string message)
+{
+    System.Console.Write(message); //вывести сообщение
+    string value = Console.ReadLine(); //считывает с консоли строку
+    int result = Convert.ToInt32(value); //преобразует строку в целое число
+    return result; //возвращает результат
+}
+//ввести массив
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int length = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
